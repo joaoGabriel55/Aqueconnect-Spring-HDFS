2fc5fa64732458ee787503c0a2f38d3eebfde69f
Gabriel Quaresma <j.quaresmasantos_98@hotmail.com>
2020-01-21 09:56:02 -0300

    Fixed relationship and made improvements

--- src/main/java/imd/smartmetropolis/aqueconnect/processors/RelationshipProcessor.java
+++ src/main/java/imd/smartmetropolis/aqueconnect/processors/RelationshipProcessor.java
@@ -1,7 +1,6 @@
 package imd.smartmetropolis.aqueconnect.processors;
 
 import com.google.gson.Gson;
-import imd.smartmetropolis.aqueconnect.dtos.RelationshipMap;
 import imd.smartmetropolis.aqueconnect.processors.hdfs.HandleHDFSFilesImpl;
 
 import java.util.*;
@@ -15,33 +14,35 @@ public class RelationshipProcessor {
     private List<ConcurrentHashMap<String, Object>> dataset1;
     private List<ConcurrentHashMap<String, Object>> dataset2;
 
-    public void makeRelationship(List<RelationshipMap> relationshipMapList) {
-        for (RelationshipMap relationshipMap : relationshipMapList) {
-            dataset1 = loadDataSets(relationshipMap.getFilePathOne());
-            dataset2 = loadDataSets(relationshipMap.getFilePathTwo());
+    public RelationshipProcessor(
+            List<ConcurrentHashMap<String, Object>> dataset1,
+            List<ConcurrentHashMap<String, Object>> dataset2
+    ) {
+        this.dataset1 = dataset1;
+        this.dataset2 = dataset2;
+    }
 
-            makeRelationshipUsingIndexProcessor(getIdentityFields(dataset1.get(0)));
+    public RelationshipProcessor makeRelationship() {
+        makeRelationshipUsingIndexProcessor(getIdentityFields(dataset1.get(0)));
+        removeRelationshipConfigAndTransientFieldsFromCollection(dataset1);
+        removeRelationshipConfigAndTransientFieldsFromCollection(dataset2);
+        return this;
+    }
 
-            dataset1 = removeRelationshipConfigAndTransientFieldsFromCollection(dataset1);
-            dataset2 = removeRelationshipConfigAndTransientFieldsFromCollection(dataset2);
-
-            HandleHDFSFilesImpl.getInstance().writeFile(relationshipMap.getFilePathOne(), new Gson().toJson(dataset1));
-            HandleHDFSFilesImpl.getInstance().writeFile(relationshipMap.getFilePathTwo(), new Gson().toJson(dataset2));
-            dataset1 = null;
-            dataset2 = null;
-        }
+    public RelationshipProcessor confirmRelationship(String filePath) {
+        HandleHDFSFilesImpl.getInstance().writeFile(filePath, new Gson().toJson(dataset1));
+        return this;
     }
 
     @SuppressWarnings("unchecked")
-    private List<String> getIdentityFields(Object data) {
+    public List<String> getIdentityFields(Object data) {
         List<String> identityFields = new ArrayList<>();
         Map<String, Object> object = (Map<String, Object>) data;
         for (Map.Entry<String, Object> objectEntry : object.entrySet()) {
             if (objectEntry.getValue() instanceof Map) {
                 Map<String, Object> objectMap = (Map<String, Object>) objectEntry.getValue();
                 if (objectMap.containsKey("relationshipConfig")) {
-                    String identifyField = (String) ((Map<String, Object>) objectMap.get("relationshipConfig"))
-                            .get("identityField");
+                    String identifyField = (String) ((Map<String, Object>) objectMap.get("relationshipConfig")).get("identityField");
                     identityFields.add(identifyField);
                 }
             }
@@ -64,9 +65,7 @@ public class RelationshipProcessor {
     }
 
     @SuppressWarnings("unchecked")
-    private void makeRelationshipUsingIndexProcessor(
-            List<String> identityFields
-    ) {
+    public void makeRelationshipUsingIndexProcessor(List<String> identityFields) {
         if (identityFields.size() > 0) {
             for (Iterator<String> iterator = identityFields.iterator(); iterator.hasNext(); ) {
                 String identityField = iterator.next();
@@ -76,8 +75,8 @@ public class RelationshipProcessor {
                     for (Iterator<Map.Entry<String, Object>> iteratorMapProps = data.entrySet().iterator(); iteratorMapProps.hasNext(); ) {
                         Map.Entry<String, Object> objectEntry = iteratorMapProps.next();
                         if (objectEntry.getValue() instanceof Map && mapIterator.hasNext()) {
-                            this.makeRelationshipUsingIndexProcessor(
-                                    dataset1.iterator().next(),
+                            boolean isMakeRelationship = this.makeRelationshipUsingIndexProcessor(
+                                    data,
                                     identityField,
                                     indexes,
                                     objectEntry.getKey(),
@@ -91,7 +90,7 @@ public class RelationshipProcessor {
     }
 
     @SuppressWarnings("unchecked")
-    private void makeRelationshipUsingIndexProcessor(
+    private boolean makeRelationshipUsingIndexProcessor(
             ConcurrentHashMap<String, Object> data,
             String identifyField,
             Map<Object, Integer> indexes,
@@ -99,8 +98,7 @@ public class RelationshipProcessor {
             Map<String, Object> value
     ) {
         if (value.get("type").equals("Relationship") && value.containsKey("relationshipConfig")) {
-            String relationshipConfigIdentifyField = (String) ((Map<String, Object>) value
-                    .get("relationshipConfig")).get("identityField");
+            String relationshipConfigIdentifyField = (String) ((Map<String, Object>) value.get("relationshipConfig")).get("identityField");
             if (relationshipConfigIdentifyField.equals(identifyField)) {
                 Object objectValue = value.get("object");
                 ConcurrentHashMap<String, Object> dataset2Element = indexes.containsKey(objectValue) ? dataset2.get(indexes.get(objectValue)) : null;
@@ -116,9 +114,11 @@ public class RelationshipProcessor {
                     if (value.containsKey("relationshipConfig")) {
                         data.remove("relationshipConfig");
                     }
+                    return true;
                 }
             }
         }
+        return false;
     }
 
     @SuppressWarnings("unchecked")
@@ -130,18 +130,32 @@ public class RelationshipProcessor {
                 if (objectEntry.getValue() instanceof Map) {
                     Map<String, Object> valueMap = (Map<String, Object>) objectEntry.getValue();
                     if (valueMap.containsKey("relationshipConfig"))
-                        ((ConcurrentHashMap<String, Object>) dataset.iterator().next()).remove("relationshipConfig");
+                        valueMap.remove("relationshipConfig");
+
+                    if (valueMap.containsKey("object") && !valueMap.get("object").toString().contains("urn:ngsi-ld"))
+                        valueMap.put("object", null);
 
                     if (valueMap instanceof Map && valueMap.get("type").equals("Transient"))
-                        ((ConcurrentHashMap<String, Object>) dataset.iterator().next()).remove(objectEntry.getKey());
+                        valueMap.remove(objectEntry.getKey());
                 }
             }
         }
         return dataset;
     }
 
-    private List<ConcurrentHashMap<String, Object>> loadDataSets(String dataSetPath) {
-        return HandleHDFSFilesImpl.getInstance().readFile(dataSetPath);
+    public List<ConcurrentHashMap<String, Object>> getDataset1() {
+        return dataset1;
     }
 
+    public void setDataset1(List<ConcurrentHashMap<String, Object>> dataset1) {
+        this.dataset1 = dataset1;
+    }
+
+    public List<ConcurrentHashMap<String, Object>> getDataset2() {
+        return dataset2;
+    }
+
+    public void setDataset2(List<ConcurrentHashMap<String, Object>> dataset2) {
+        this.dataset2 = dataset2;
+    }
 }
--- src/main/java/imd/smartmetropolis/aqueconnect/resources/RelationshipResource.java
+++ src/main/java/imd/smartmetropolis/aqueconnect/resources/RelationshipResource.java
@@ -1,7 +1,9 @@
 package imd.smartmetropolis.aqueconnect.resources;
 
 import imd.smartmetropolis.aqueconnect.dtos.DataSetRelationship;
+import imd.smartmetropolis.aqueconnect.dtos.RelationshipMap;
 import imd.smartmetropolis.aqueconnect.processors.RelationshipProcessor;
+import imd.smartmetropolis.aqueconnect.processors.hdfs.HandleHDFSFilesImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -18,10 +20,17 @@ public class RelationshipResource {
 
     @PostMapping(value = "/relationships")
     public ResponseEntity<String> makeRelationships(@RequestBody DataSetRelationship dataSetRelationship) {
-        RelationshipProcessor processor = new RelationshipProcessor();
+
 
         try {
-            processor.makeRelationship(dataSetRelationship.getRelationshipMap());
+            for (RelationshipMap relationshipMap : dataSetRelationship.getRelationshipMap()) {
+                new RelationshipProcessor(
+                        HandleHDFSFilesImpl.getInstance().readFile(relationshipMap.getFilePathOne()),
+                        HandleHDFSFilesImpl.getInstance().readFile(relationshipMap.getFilePathTwo()))
+                        .makeRelationship()
+                        .confirmRelationship(relationshipMap.getFilePathOne())
+                        .confirmRelationship(relationshipMap.getFilePathTwo());
+            }
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
